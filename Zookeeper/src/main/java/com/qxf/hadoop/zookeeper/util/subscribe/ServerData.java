4b8d17fef4ef9dd1bd1ca778a8db216c54200f66
qiuxuefu <qiuxuefu376@pingan.com.cn>
2019-04-11 20:18:43 +0800

    zookeeper 服务注册发现 配置管理

---
+++ Zookeeper/src/main/java/com/qxf/hadoop/zookeeper/util/subscribe/ServerData.java
@@ -0,0 +1,42 @@
+package com.qxf.hadoop.zookeeper.util.subscribe;
+
+/**
+ * 记录WordServer的基本信息
+ * 
+ */
+public class ServerData {
+
+	private Integer id;
+	private String name;
+	private String address;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "ServerData [id=" + id + ", name=" + name + ", address=" + address + "]";
+	}
+
+}
